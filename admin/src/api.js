2e93c3c359fd2590b7ac755e385cf4a433505ca2
russ <1921615+thatrussguy@users.noreply.github.com>
2021-09-12 12:30:47 +0100

    add api helpers

--- admin/src/api.js
+++ admin/src/api.js
@@ -1,13 +1,26 @@
 const axios = require("axios")
 const config = require("config")
 
+const financialCompaniesServiceAPI = axios.create({
+  baseURL: `${config.financialCompaniesServiceUrl}/companies`,
+})
 const investmentsServiceAPI = axios.create({
   baseURL: `${config.investmentsServiceUrl}/investments`,
 })
+
+const fetchCompanies = async () => {
+  const {data: companies} = await financialCompaniesServiceAPI.get()
+  return companies
+}
 
 const fetchInvestmentById = async (id) => {
   const {data: investment} = await investmentsServiceAPI.get(`/${id}`)
   return investment
 }
 
-module.exports = {fetchInvestmentById}
+const fetchInvestments = async () => {
+  const {data: investments} = await investmentsServiceAPI.get()
+  return investments
+}
+
+module.exports = {fetchCompanies, fetchInvestmentById, fetchInvestments}
